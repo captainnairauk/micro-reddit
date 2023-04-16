8b17261f1fc2ee08e3d11928cdec44f9b5bbdc4c
Aniket Kaimal <aniket1995@yahoo.co.in>
2023-04-16 14:27:17 +0530

    Add validations, associations on comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
 
   validates :username, presence: true, uniqueness: { case_sensitive: true }, length: {in: 4..12}
   validates :email, presence: true, uniqueness: { case_sensitive: true }
