36ffcce5f40458ff1f2f0151563b229ffd907618
Aniket Kaimal <aniket1995@yahoo.co.in>
2023-04-16 14:13:05 +0530

    Add Comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :username, presence: true, uniqueness: { case_sensitive: true }, length: {in: 4..12}
   validates :email, presence: true, uniqueness: { case_sensitive: true }
   validates :password, presence: true, length: {in: 6..16}
